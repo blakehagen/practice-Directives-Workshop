e40ab78c2ae302d99d04713a79fbc5d69e6989f2
Blake Hagen <blakehagen@gmail.com>
2015-02-19 20:03:59 -0700

    thur 8pm

---
+++ myDirectives.js
@@ -0,0 +1,13 @@
+var app = angular.module('myDirectives');
+
+app.directive('pending', function() {
+	return {
+		restrict: 'AE',
+		scope: {},
+		link: function($scope, elem, attrs) {
+
+		}
+	}
+
+
+});
